32627909c1cc14cd2c01582bc22bce1c75f56352
Stallone2K <stallonefernandess@gmail.com>
2026-05-08 14:08:17 +0530

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://stallone2k.github.io",
   devToolbar: {
     enabled: false,
   },
